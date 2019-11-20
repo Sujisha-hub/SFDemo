ee883a43cbba4e4826b7b635b777d21b860f4885
https://github.com/Sujisha-hub/SalesforceDemo <SChandrasekar@arrayasolutions.com>
2019-11-20 18:55:14 +0530

    SFDemoCommit

---
+++ force-app/main/default/aura/PrintHelloWorld/PrintHelloWorldController.js
@@ -0,0 +1,15 @@
+({
+    handleClick : function(component, event, helper) {
+
+        //var action = component.get("c.ApexClass"); 
+        //action.setParams({"recordId":recordId,"oppStage":oppStages,"oppDate":oppDates,"closureAmount":closingAmount});
+        //action.setCallback(this, function(response) {
+
+            //var res=response.getReturnValue();
+          //  component.set("v.Message",res);
+
+      ////  });
+       // $A.enqueueAction(action); 
+
+    }
+})
